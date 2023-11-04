1e0affec5a670a951f5ea8b2f405969537cfd7ad
Eugene Fedosov <wendomen@yandex.ru>
2023-11-04 12:30:04 +0300

    a restricted area for movement has been introduced

--- app/src/main/java/com/app/osm_as_kotlin/MainActivity.kt
+++ app/src/main/java/com/app/osm_as_kotlin/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import org.osmdroid.api.IMapController
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.BoundingBox
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,11 +23,21 @@ class MainActivity : AppCompatActivity() {
         mapView = findViewById(R.id.mapView)
         mapView.setTileSource(TileSourceFactory.MAPNIK)
 
+        // стартовая точка
         val mapController: IMapController = mapView.controller
         mapController.setZoom(17.0)
         mapController.setCenter(GeoPoint(60.0071, 30.3720))
 
         mapView.setMultiTouchControls(true)
+
+        // ограничение области
+        val northBoundary = 60.01520
+        val southBoundary = 59.99299
+        val eastBoundary = 30.39141
+        val westBoundary = 30.35188
+        val boundingBox = BoundingBox(northBoundary, eastBoundary, southBoundary, westBoundary)
+        // mapView.setScrollableAreaLimit(boundingBox)
+        mapView.setScrollableAreaLimitDouble(boundingBox)
     }
 
     override fun onResume() {
